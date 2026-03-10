e3a3d310b7efd624ea98ac9e8a0fbf1e44a4dd0e
Mk <munkyuseo99@gmail.com>
2026-03-10 21:33:03 +0900

    main.ejs 에 버튼 추가해서 inventory.ejs로 페이지 이동까지 구현했습니다

--- src/controllers/inventory.controller.js
+++ src/controllers/inventory.controller.js
@@ -1,8 +1,13 @@
 const inventoryService = require('../services/inventory.service');
 
-const getProducts = (req, res) => {
-    const data = inventoryService.getAllProducts();
-    res.json(data);
+const getProducts = async (req, res)=> {
+
+    //todo 재고 관리 API 생성 
+     const result = await inventoryService.getAllProducts();
+
+     console.log(result);
+
+    res.render('inventory' , {result} );
 };
 
 module.exports = {
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const express = require('express');
 // 메인 도메인(게시글 목록/상세 등)과 관련된 하위 라우터를 불러옵니다.
 // 실제 개별 경로 정의는 ./main.route.js 에서 처리됩니다.
 const mainRoute = require('./main.route');
+const inventoryRoute = require('./inventory.route');
 
 // 하위 라우터들을 묶어 외부로 내보낼 상위 라우터 객체를 생성합니다.
 const router = express.Router();
@@ -15,5 +16,7 @@ const router = express.Router();
 // '/'로 들어오는 모든 요청을 mainRoute에게 위임합니다.
 // 예) GET '/' → mainRoute.index, GET '/detail/:id' → mainRoute.detail 등
 router.use('/', mainRoute);
+router.use('/inventory', inventoryRoute);
+
 // 이 라우터를 모듈로 내보내서 app.js에서 사용합니다.
 module.exports = router;
