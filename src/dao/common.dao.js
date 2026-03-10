794a48d1589702d3a2bf2df71b64bed1e0b83e28
jinah <wlsdk6395@naver.com>
2026-03-10 20:26:15 +0900

    Auth -> AUTH 수정

--- src/dao/common.dao.js
+++ src/dao/common.dao.js
@@ -88,7 +88,7 @@ const commonDao = async (mapperId, sqlId, params) => {
 // 서비스 계층에서 mapperId를 쉽게 사용할 수 있도록 네임스페이스를 상수로 정의합니다.
 const mapper = {
     MAIN: "main",
-    Auth: "auth",
+    AUTH: "auth",
 }
 
 // 다른 파일에서 commonDao 함수와 mapper 객체를 사용할 수 있도록 export 합니다.
