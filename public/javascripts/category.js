5fc5c6c2f79ce2c120f3ec103f82023cc31524c1
corud170612 <prettycorud@gmail.com>
2026-05-14 00:00:33 +0900

    제품추가 modal x버튼 style 못 읽는 오류 수정, 카테고리 추가 공란이면 alert창 띄우고 retrun false주기

--- public/javascripts/category.js
+++ public/javascripts/category.js
@@ -1,17 +1,36 @@
 function deleteCategory(number) {
-    console.log($('.cateId').get(number).textContent);
+    // console.log($('.cateId').get(number).textContent);
     // console.log(number);
-    $.ajax({
-        method: 'POST',
-        url: '/product/deleteCategory',
-        data: { cateId: $('.cateId').get(number).textContent },
-        success: function () {
-            alert("성공적으로 삭제하였습니다")
-            location.reload();
-        },
-        error: function () {
-            alert("에러발생")
-        }
-    })
 
+    if (confirm("정말로 삭제하시겠습니까?")) {
+        $.ajax({
+            method: 'POST',
+            url: '/product/deleteCategory',
+            data: { cateId: $('.cateId').get(number).textContent },
+            success: function () {
+                alert("성공적으로 삭제하였습니다")
+                location.reload();
+            },
+            error: function () {
+                alert("에러발생")
+            }
+        })
+    } else {
+        return false;
+    }
+
+
+
+}
+
+function doSubmit(){
+    var frm = document.searchForm;
+
+    if(!frm.cateName.value.trim()){
+        alert("카테고리 이름을 입력하세요");
+        frm.cateName.focus();
+        return false;
+    }
+    
+    return ture;
 }
--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,19 +1,15 @@
-const btn = document.getElementById("serchButton");
 const modal = document.getElementById("modalWrap");
 const closeBtn = document.getElementById("closeBtn");
 
 
-btn.onclick = function () {
-//   alert('hey');
-   modal.style.display = "block"; // 버튼을 클릭하면 모달을 보이게 함
-};
+function addProduct(){
+  // alert("hey");
+  const modal = document.getElementById("modalWrap");
+  modal.style.display = "block"; // 버튼을 클릭하면 모달을 보이게 함
+}
 
- closeBtn.onclick = function () {
-   modal.style.display = "none"; // 모달을 닫는 버튼(X)을 클릭하면 모달을 숨김
- };
-
- window.onclick = function (event) {
-   if (event.target == modal) {
-     modal.style.display = "none"; // 모달 외부를 클릭하면 모달을 숨김
-   }
- };
+function closeModal(){
+  const modal = document.getElementById("modalWrap");
+  const closeBtn = document.getElementById("closeBtn");
+  modal.style.display = "none"; // 모달을 닫는 버튼(X)을 클릭하면 모달을 숨김
+}
